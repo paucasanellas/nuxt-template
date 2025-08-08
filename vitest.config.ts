962410ca0d10a6de54c1d21d32862cd4faf62362
Pau Casanellas <paucasanellas@icloud.com>
2025-08-08 11:51:20 +0200

    feat(chore): add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './tests/unit/coverage',
+    },
     projects: [
       {
         test: {
