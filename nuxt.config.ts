719f9a569d19f367c513ef6e8743786da55b3528
Pau Casanellas <paucasanellas@icloud.com>
2025-08-08 11:35:32 +0200

    feat(chore): add basic nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,16 @@
 export default defineNuxtConfig({
+  components: [
+    { path: '@/components', pathPrefix: false },
+  ],
   devtools: {
     enabled: import.meta.env.DEVTOOLS_ENABLED || false,
     timeline: {
       enabled: true,
     },
   },
+  css: ['@/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
+  telemetry: {
+    enabled: false,
+  },
 })
